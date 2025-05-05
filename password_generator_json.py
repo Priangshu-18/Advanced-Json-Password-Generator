7e2321eee08a23f7b7a7de9f28cef2184f8542db
Priangshu Mukherjee <157562695+Priangshu-18@users.noreply.github.com>
2025-05-05 12:10:53 +0530

    Update password_generator_json.py

--- password_generator_json.py
+++ password_generator_json.py
@@ -13,11 +13,12 @@ def generate_password():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-     # Randomly decide how many of each character type to include
+    # Randomly decide how many of each character type to include
     nr_letters = random.randint(8, 10)
     nr_symbols = random.randint(2, 4)
     nr_numbers = random.randint(2, 4)
-    
+
+    # Randomly select the characters
     password_letters = [random.choice(letters) for _ in range(nr_letters)]
     password_symbols = [random.choice(numbers) for _ in range(nr_numbers)]
     password_numbers = [random.choice(symbols) for _ in range(nr_symbols)]
